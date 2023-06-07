4494cdce92c887d340751963cf8d9a2810595b51
ShutoTadokoro <tadokoro.shuto.infratp@gmail.com>
2023-06-07 07:18:54 +0000

    1.認証機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'public/homes#top'
+  get '/about' => 'public/homes#about'
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    #registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+  
+  get '/customers' => 'public/customers#show'
+  
+  namespace :admin do
+        get '/' => 'homes#top'
+  end
+  
 end
